1f25f7991483ffed1092973c577f94c34ec93097
George Courtsunis <gjcourt@gmail.com>
2015-08-14 14:10:39 -0700

    Updates bootstrap ignore files.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 function dotfiles {
-    ls -a | egrep -v "^\.{1,2}$" | grep -e "^\."
+    ls -a | grep -vE "^\.{1,2}$" | grep -E "^\." | grep -vE '^.git$'
 }
 for file in `dotfiles`
 do
